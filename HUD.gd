77ad09b774ab43875c40058e3849fc648b2f2593
Cyanael <tatiana.rocher@gmail.com>
2020-10-09 23:17:13 +0100

    Main + HUD
    ajout des bouttons et labels de HUD + ajout des polices d'écriture
    ajout MAIN
    lien Main-HUD

---
+++ HUD.gd
@@ -0,0 +1,30 @@
+extends CanvasLayer
+
+signal start_game
+
+func show_message(text):
+	$Titre.text = text
+	$Titre.show()
+	
+func show_game_over():
+	show_message("Thanks for playing")
+# Called when the node enters the scene tree for the first time.
+
+func update_money(value):
+	$Money.text = str(int($Money.text)+value)
+
+func new_game():
+	$Titre.show()
+	$StartButton.show()
+
+func _on_Start_pressed():
+	$Titre.hide()
+	$StartButton.hide()
+	$Lore.show()
+	$NextLoreButton.show()
+
+
+func _on_NextLoreButton_pressed():
+	$Lore.hide()
+	$NextLoreButton.hide()
+	emit_signal("start_game")
